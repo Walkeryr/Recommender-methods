577e296033dfcf6b31bd5297f69420eaf758bb5a
Yuriy Khamzyaev <yuri.rhm@gmail.com>
2012-03-29 16:49:34 +0400

    Finished function for data preparation and saving

--- prepare_data.m
+++ prepare_data.m
@@ -1,6 +1,12 @@
 fid = fopen('../data/movielens/ratings.dat');
 ratings_cell = textscan(fid, '%f %f %f %f', 'delimiter', ':', ... 
                            'CollectOutput', 1, 'MultipleDelimsAsOne', 1);
-ratings_raw = ratings_cell{1};
-ratings = sparse(max(ratings_raw(:,1)), max(ratings_raw(:,2)));
-fclose(fid);                                 
+fclose(fid);       
+raw_mat = ratings_cell{1};
+ratings = sparse(raw_mat(:,1), raw_mat(:,2), raw_mat(:,3), ... 
+                                   max(raw_mat(:,1)), max(raw_mat(:,2)));
+                               
+save ../data/movielens/ratings.mat ratings;
+                       
+
+
