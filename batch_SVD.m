cacede43a4f5004bca272e78daa075058186ce91
Yuriy Khamzyaev <yuri.rhm@gmail.com>
2012-03-31 19:14:39 +0400

    Finished vectorized batch SVD algorithm with perfomance measure RMSE = 1.28

--- batch_SVD.m
+++ batch_SVD.m
@@ -1,14 +1,21 @@
-load ../data/movielens/ml-1m/ratings.mat
-
-f = 3; % number of factors
+load ../data/movielens/ml-100k/u1_base.mat
+load ../data/movielens/ml-100k/u1_test.mat
+f = 20; % number of factors
 U = create_and_fill(ratings,f,length(ratings(:,1)));
 M = create_and_fill(ratings,f,length(ratings(1,:)));
 ku = 0.05;
 km = 0.05;
-mu = 0.01; % learning rate
+mu = 0.0005; % learning rate
 a = min(nonzeros(ratings)); % min rating
 b = max(nonzeros(ratings)); % max rating
 
-gradUtic = tic;
-gradU = gradient_U_vect(ratings,U,M,ku);
-toc(gradUtic)
+for i = 1:100
+    gradU = gradient_U_vect(ratings,U,M,ku);
+    gradM = gradient_M_vect(ratings,U,M,km);
+    U = U + mu * gradU;
+    M = M + mu * gradM;
+    V = U' * M;
+    RMSE(V,test_ratings)
+end
+
+%plot(1:10, RMSE_arr)
--- prepare_data.m
+++ prepare_data.m
@@ -1,12 +1,12 @@
-fid = fopen('../data/movielens/ml-100k/u1.base');
+fid = fopen('../data/movielens/ml-100k/u1.test');
 ratings_cell = textscan(fid, '%f %f %f %f', 'delimiter', ' ', ... 
                            'CollectOutput', 1, 'MultipleDelimsAsOne', 1);
 fclose(fid);       
 raw_mat = ratings_cell{1};
-ratings = sparse(raw_mat(:,1), raw_mat(:,2), raw_mat(:,3), ... 
+test_ratings = sparse(raw_mat(:,1), raw_mat(:,2), raw_mat(:,3), ... 
                                    max(raw_mat(:,1)), max(raw_mat(:,2)));
                                
-save ../data/movielens/ml-100k/u1_base.mat ratings;
+save ../data/movielens/ml-100k/u1_test.mat test_ratings;
                        
 
 
