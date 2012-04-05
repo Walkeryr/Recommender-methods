b33ffaf20afc563fe081070b638246b05eb0be71
Yuriy Khamzyaev <yuri.rhm@gmail.com>
2012-04-05 11:42:38 +0400

    Finished batch SVD vectorized method

--- batch_SVD.m
+++ batch_SVD.m
@@ -1,21 +1,33 @@
-load ../data/movielens/ml-100k/u1_base.mat
-load ../data/movielens/ml-100k/u1_test.mat
-f = 20; % number of factors
+load ../data/movielens/ml-1m/ratings.mat
+load ../data/movielens/ml-1m/test_rat.mat
+f = 60; % number of factors
 U = create_and_fill(ratings,f,length(ratings(:,1)));
 M = create_and_fill(ratings,f,length(ratings(1,:)));
 ku = 0.05;
 km = 0.05;
-mu = 0.0005; % learning rate
+mu = 0.0001; % learning rate
 a = min(nonzeros(ratings)); % min rating
 b = max(nonzeros(ratings)); % max rating
+RMSE_vals = [];
+iter_times = [];
+iters = 0;
+prev_RMSE = 100;
+curr_RMSE = 50;
 
-for i = 1:100
+while ((prev_RMSE - curr_RMSE) > 0)
+    iters = iters + 1;
+    tstart = tic;
     gradU = gradient_U_vect(ratings,U,M,ku);
     gradM = gradient_M_vect(ratings,U,M,km);
     U = U + mu * gradU;
     M = M + mu * gradM;
-    V = U' * M;
-    RMSE(V,test_ratings)
+    V = U' * M;    
+    prev_RMSE = curr_RMSE;
+    curr_RMSE = RMSE(V,test_ratings); 
+    RMSE_vals = [RMSE_vals curr_RMSE];    
+    iter_times = [iter_times toc(tstart)];
 end
 
-%plot(1:10, RMSE_arr)
+plot(1:length(RMSE_vals),RMSE_vals)
+xlabel('iteration');
+ylabel('RMSE');
