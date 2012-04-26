0c230e5453d140dbc4ce802aecc53284887c8dbb
Yuriy Khamzyaev <yuri.rhm@gmail.com>
2012-04-26 18:19:10 +0400

    Began implementing ranking CF algorithm

---
+++ ranking_CF/prepare_data.m
@@ -0,0 +1,50 @@
+fid = fopen('../../data/movielens/ml-100k/u.data');
+ratings_cell = textscan(fid, '%f %f %f %f', 'delimiter', '::', ... 
+                           'CollectOutput', 1, 'MultipleDelimsAsOne', 1);
+fclose(fid);       
+raw_mat = ratings_cell{1};
+% matrix of ratings of users x items
+ratings = sparse(raw_mat(:,1), raw_mat(:,2), raw_mat(:,3), ... 
+                                   max(raw_mat(:,1)), max(raw_mat(:,2)));
+ratings_backup = ratings;
+
+n = length(ratings(:,1));    % number of users                               
+allpairprefs = cell(1,n);    % cell array of matrices containing pairwise
+                             % preferences of all users
+max_rating = max(raw_mat(:,3));
+min_rating = min(raw_mat(:,3));
+                          
+% computing pairwise preferences for each user                          
+for i = 1:n
+    prefs = []; % pairwise preferences for one user
+    while (curr_max ~= min_rating)
+        curr_max = max(ratings(i,:)); 
+        a = find(ratings(i,:) == curr_max);
+        b = find(ratings(i,:) < curr_max & ratings(i,:) ~= 0);
+        prefs = [prefs; allcomb(a,b)];
+    end
+    allpairprefs{i} = prefs; 
+end
+                               
+                               
+                               
+                               
+                               
+                               
+                               
+%n = length(raw_mat(:,1));
+%n_test = floor(0.2 * n);
+%test_ratings = sparse(length(ratings(:,1)),length(ratings(1,:)));
+
+%[nzr,nzc] = find(ratings); % nzr - nonzero row, nzc - nonzero column
+%idx = sub2ind(size(ratings), nzr, nzc);
+%n = numel(idx);
+%p = randperm(n);
+%test_ratings(idx(p(1:n_test))) = ratings(idx(p(1:n_test)));
+%ratings(idx(p(1:n_test))) = 0;
+
+%save ../../data/movielens/ml-1m/test_rat.mat test_ratings;
+%save ../../data/movielens/ml-1m/ratings.mat ratings;
+                       
+
+
